d5a247159182cfa99fc024b6d975d9ecfb18f410
simon-db <freelance.simon.db@gmail.com>
2013-08-22 18:22:28 +0300

    Add custom processors
    Add scale to optimal size without crop
    Add watermark

--- easy_thumbnails/processors.py
+++ easy_thumbnails/processors.py
@@ -234,3 +234,226 @@ def filters(im, detail=False, sharpen=False, **kwargs):
     if sharpen:
         im = im.filter(ImageFilter.SHARPEN)
     return im
+    
+# Custom processors
+
+def custom_scale(im, size, **kwargs):
+    source_x, source_y = [float(v) for v in im.size]
+    target_x, target_y = [float(v) for v in size]
+
+    if not target_x or not target_y:
+        scale = max(target_x / source_x, target_y / source_y)
+    else:
+        scale = min(target_x / source_x, target_y / source_y)
+
+    if scale < 1.0 :
+        # Resize the image to the target size boundary. Round the scaled
+        # boundary sizes to avoid floating point errors.
+        im = im.resize((int(round(source_x * scale)),
+                        int(round(source_y * scale))),
+                       resample=Image.ANTIALIAS)
+    return im
+
+def prefect_scale(im, size, prefect=False, **kwargs):
+    if prefect:
+        im = custom_scale(im, (size[0], 0))
+        im = custom_scale(im, (0, size[1]))
+    return im
+
+
+def _percent(var):
+    """
+    Just a simple interface to the _val function with a more meaningful name.
+    """
+    return _val(var, True)
+
+def _int(var):
+    """
+    Just a simple interface to the _val function with a more meaningful name.
+    """
+    return _val(var)
+
+def _val(var, is_percent=False):
+    """
+    Tries to determine the appropriate value of a particular variable that is
+    passed in.  If the value is supposed to be a percentage, a whole integer
+    will be sought after and then turned into a floating point number between
+    0 and 1.  If the value is supposed to be an integer, the variable is cast
+    into an integer.
+    """
+    try:
+        if is_percent:
+            var = float(int(var.strip('%')) / 100.0)
+        else:
+            var = int(var)
+    except ValueError:
+        raise ValueError('invalid watermark parameter: ' + var)
+    return var
+
+def reduce_opacity(img, opacity):
+    """
+    Returns an image with reduced opacity.
+    """
+    assert opacity >= 0 and opacity <= 1
+
+    if img.mode != 'RGBA':
+        img = img.convert('RGBA')
+    else:
+        img = img.copy()
+
+    alpha = img.split()[3]
+    alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
+    img.putalpha(alpha)
+
+    return img
+
+def determine_scale(scale, img, mark):
+    """
+    Scales an image using a specified ratio or 'F'.  If `scale` is 'F', the
+    image is scaled to be as big as possible to fit in `img` without falling off
+    the edges.  Returns the scaled `mark`.
+    """
+
+    if scale:
+        try:
+            scale = float(scale)
+        except (ValueError, TypeError):
+            pass
+
+        if type(scale) in (str, unicode) and scale.lower() == 'f':
+            # scale, but preserve the aspect ratio
+            scale = min(
+                        float(img.size[0]) / mark.size[0],
+                        float(img.size[1]) / mark.size[1]
+                       )
+        elif type(scale) not in (float, int):
+            raise ValueError('Invalid scale value "%s"!  Valid values are 1) "F" for ratio-preserving scaling and 2) floating-point numbers and integers greater than 0.' % (scale,))
+
+        # determine the new width and height
+        w = int(mark.size[0] * float(scale))
+        h = int(mark.size[1] * float(scale))
+
+        # apply the new width and height, and return the new `mark`
+        return (w, h)
+    else:
+        return mark.size
+
+def determine_position(position, img, mark):
+    """
+    Options:
+        TL: top-left
+        TR: top-right
+        BR: bottom-right
+        BL: bottom-left
+        C: centered
+        R: random
+        X%xY%: relative positioning on both the X and Y axes
+        X%xY: relative positioning on the X axis and absolute positioning on the
+              Y axis
+        XxY%: absolute positioning on the X axis and relative positioning on the
+              Y axis
+        XxY: absolute positioning on both the X and Y axes
+    """
+
+    max_left = max(img.size[0] - mark.size[0], 0)
+    max_top = max(img.size[1] - mark.size[1], 0)
+
+    if not position:
+        position = 'r'
+
+    if isinstance(position, tuple):
+        left, top = position
+    elif isinstance(position, str) or isinstance(position, unicode):
+        position = position.lower()
+
+        # corner positioning
+        if position in ['tl', 'tr', 'br', 'bl']:
+            if 't' in position:
+                top = 0
+            elif 'b' in position:
+                top = max_top
+            if 'l' in position:
+                left = 0
+            elif 'r' in position:
+                left = max_left
+
+        # center positioning
+        elif position == 'c':
+            left = int(max_left / 2)
+            top = int(max_top / 2)
+
+        # random positioning
+        elif position == 'r':
+            left = random.randint(0, max_left)
+            top = random.randint(0, max_top)
+
+        # relative or absolute positioning
+        elif 'x' in position:
+            left, top = position.split('x')
+
+            if '%' in left:
+                left = max_left * _percent(left)
+            else:
+                left = _int(left)
+
+            if '%' in top:
+                top = max_top * _percent(top)
+            else:
+                top = _int(top)
+    return (int(left), int(top))
+
+
+
+def watermark(img, mark, position=(0, 0), opacity=1, scale=1.0, tile=False, **kwargs):
+    if opacity < 1:
+        mark = reduce_opacity(mark, opacity)
+
+    if type(scale) != tuple:
+        scale = determine_scale(scale, img, mark)
+
+    mark = mark.resize(scale)
+    position = determine_position(position, img, mark)
+
+    if img.mode != 'RGBA':
+        img = img.convert('RGBA')
+
+    # make sure we have a tuple for a position now
+    assert isinstance(position, tuple), 'Invalid position "%s"!' % position
+
+    # create a transparent layer the size of the image and draw the
+    # watermark in that layer.
+    layer = Image.new('RGBA', img.size, (0,0,0,0))
+    if tile:
+        first_y = position[1] % mark.size[1] - mark.size[1]
+        first_x = position[0] % mark.size[0] - mark.size[0]
+
+        for y in range(first_y, img.size[1], mark.size[1]):
+            for x in range(first_x, img.size[0], mark.size[0]):
+                layer.paste(mark, (x, y))
+    else:
+        layer.paste(mark, position)
+    return Image.composite(layer, img, layer)
+
+def add_watermark(im, size, crop=False, mark=False, **kwargs):
+    if mark:
+        path = getattr(settings, 'MEDIA_ROOT')+mark
+        if os.path.exists(path):
+            try:
+                wmark = Image.open(path)
+                position=(0,0)
+                opacity=1
+                scale=1.0
+
+                if 'position' in kwargs:
+                    position = kwargs['position']
+
+                if 'opacity' in kwargs:
+                    opacity = kwargs['opacity']
+
+                if 'scale' in kwargs:
+                    scale = kwargs['scale']
+
+                im = watermark(im, wmark, position, opacity, scale, **kwargs)
+            except:
+                pass
+    return im
